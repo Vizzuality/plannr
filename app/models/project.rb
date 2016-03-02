f5940f79d2808a044231c5e05a75fc69f5700939
Simao Belchior <simaobelchior@gmail.com>
2016-03-02 23:29:39 +0000

    Fixes display of outlook

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,12 @@
 class Project < ActiveRecord::Base
+
+  def status_for date
+    if end_date.beginning_of_month <= date
+      "finished"
+    elsif start_date.beginning_of_month <= date
+      "underway"
+    else
+      "not-started"
+    end
+  end
 end
