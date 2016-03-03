d4560dccf8914bdc440d6ef64efb5dd811c19acf
Simao Belchior <simaobelchior@gmail.com>
2016-03-03 16:01:40 +0000

    Make sure javascript works between pages

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,9 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-
-$(function() {
+var ready;
+ready = function() {
   $('.datepicker').datepicker({
     format: 'dd-mm-yyyy'
   });
   $('[data-toggle="tooltip"]').tooltip()
-});
+};
+$(document).ready(ready);
+$(document).on('page:load', ready);
