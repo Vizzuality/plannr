60c85bf08d6011bbaa7b4ed8643c87f3da6bdbe1
Simao Belchior <simaobelchior@gmail.com>
2016-09-23 19:52:38 +0200

    Display roster in teams page

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,5 +2,8 @@ class Team < ApplicationRecord
   belongs_to :project_manager, class_name: 'User'
   belongs_to :tech_lead, class_name: 'User'
 
+  has_many :rosters
+  has_many :users, through: :rosters
+
   scope :active, -> { where(active: true) }
 end
