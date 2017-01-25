10f77723e3f7f4b358c53f685712806de4cb3ff2
Simao Belchior <simaobelchior@gmail.com>
2017-01-25 11:55:45 +0000

    Show weeks

--- app/controllers/outlook_controller.rb
+++ app/controllers/outlook_controller.rb
@@ -2,8 +2,8 @@ class OutlookController < ApplicationController
   before_action :set_teams_and_project_managers, only: [:index]
 
   def index
-    @interval = [3.months.ago.beginning_of_month.to_date,
-                 6.months.from_now.end_of_month.to_date]
+    @interval = [1.months.ago.beginning_of_month.to_date,
+                 4.months.from_now.end_of_month.to_date]
     @range_of_months = range_of_months(@interval)
     @projects = Project.order(:name).
       where.not(start_date: nil, end_date: nil).
@@ -16,16 +16,7 @@ class OutlookController < ApplicationController
 
     def range_of_months interval
       start_date, end_date = interval
-      range = []
-      (start_date.year..end_date.year).each do |y|
-        mo_start = (start_date.year == y) ? start_date.month : 1
-        mo_end = (end_date.year == y) ? end_date.month : 12
-
-        (mo_start..mo_end).each do |m|
-          range << Date.parse("#{m}/#{y}")
-        end
-      end
-      range
+      (start_date..end_date).select(&:monday?)
     end
 
     def index_filters
